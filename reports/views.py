4fc18dfab56f2faf46a4a0d3640f3fad64d60f19
unknown <mngfagerlund@gmail.com>
2017-11-30 16:55:47 +0100

    First deploy

---
+++ reports/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from django.template import loader
+
+def index(request):
+    #return HttpResponse("Hello, world. You're at the reports index.")
+    template = loader.get_template('reports/index.html')
+    context = {'test_variable': 'some text'}
+    return HttpResponse(template.render(context, request))
+
+def uppgift1(request):
+    #return HttpResponse("Hello, world. You're at the reports index.")
+    template = loader.get_template('reports/uppgift1.html')
+    context = {'test_variable': 'some text'}
+    return HttpResponse(template.render(context, request))
